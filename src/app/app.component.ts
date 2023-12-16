d6288afa55bf9cdb87dcfe4a6a10cf1597ca1949
maxange-developer <massiangelone73@gmail.com>
2023-12-16 16:41:53 +0100

    fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Cart } from "./models/cart.model";
+import { CartService } from "./services/cart.service";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <app-header [cart]="cart"></app-header>
     <router-outlet></router-outlet>
   `,
-  styles: []
+  styles: [],
 })
-export class AppComponent {
-  title = 'first-angular-store';
+export class AppComponent implements OnInit {
+  cart: Cart = { items: [] };
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+    });
+  }
 }
---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from "@angular/core";
+import { Cart, CartItem } from "../../models/cart.model";
+import { CartService } from "../../services/cart.service";
+import { HttpClient } from "@angular/common/http";
+import { loadStripe } from "@stripe/stripe-js";
+
+@Component({
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+})
+export class CartComponent implements OnInit {
+  cart: Cart = {
+    items: [
+      {
+        product: "https://via.placeholder.com/150",
+        name: "sneakers",
+        price: 150,
+        quantity: 1,
+        id: 1,
+      },
+      {
+        product: "https://via.placeholder.com/150",
+        name: "shoes",
+        price: 60,
+        quantity: 3,
+        id: 2,
+      },
+    ],
+  };
+
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    "product",
+    "name",
+    "price",
+    "quantity",
+    "total",
+    "action",
+  ];
+
+  constructor(private cartService: CartService, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    });
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http
+      .post("https://localhost:4242/checkout", {
+        items: this.cart.items,
+      })
+      .subscribe(async (res: any) => {
+        let stripe = await loadStripe(
+          "pk_test_51K6jXsIFFO3AmW5AXBX7MJWxwNGqathdwdproORqcMKyRSB8jsOkeAlSqfa426SmURUTtgb2yQUNcvw8hVGbPjxg005bqK6vin"
+        );
+        stripe?.redirectToCheckout({
+          sessionId: res.id,
+        });
+      });
+  }
+}
